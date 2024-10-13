4504b9ea1ab43f5c4bd18c2f083f10ea44290e6c
Felix Kahle <felix.kahle21@gmail.com>
2024-10-13 22:10:05 +0200

    added distance callback

--- Cencora.TransportWeb.VehicleRouting/src/Solver/OrTools/Callbacks/DistanceCallback.cs
+++ Cencora.TransportWeb.VehicleRouting/src/Solver/OrTools/Callbacks/DistanceCallback.cs
@@ -9,9 +9,9 @@ using Cencora.TransportWeb.VehicleRouting.Solver.OrTools.Abstractions.Nodes;
 namespace Cencora.TransportWeb.VehicleRouting.Solver.OrTools.Callbacks;
 
 /// <summary>
-/// Represents a callback for the time.
+/// Represents a callback for the distance.
 /// </summary>
-internal sealed class TimeCallback : TransitCallbackBase
+internal sealed class DistanceCallback : TransitCallbackBase
 {
     /// <summary>
     /// The route matrix.
@@ -19,44 +19,43 @@ internal sealed class TimeCallback : TransitCallbackBase
     private readonly IReadOnlyDirectedRouteMatrix _routeMatrix;
     
     /// <summary>
-    /// Initializes a new instance of the <see cref="TimeCallback"/> class.
+    /// Initializes a new instance of the <see cref="DistanceCallback"/> class.
     /// </summary>
-    /// <param name="registrant">The callback registrant.</param>
+    /// <param name="callbackRegistrant">The callback registrant.</param>
     /// <param name="routeMatrix">The route matrix.</param>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="routeMatrix"/> is null.</exception>
-    internal TimeCallback(ICallbackRegistrant registrant, IReadOnlyDirectedRouteMatrix routeMatrix) 
-        : base(registrant)
+    public DistanceCallback(ICallbackRegistrant callbackRegistrant, IReadOnlyDirectedRouteMatrix routeMatrix) 
+        : base(callbackRegistrant)
     {
-        ArgumentNullException.ThrowIfNull(registrant, nameof(registrant));
         ArgumentNullException.ThrowIfNull(routeMatrix, nameof(routeMatrix));
         
         _routeMatrix = routeMatrix;
     }
-    
+
     /// <inheritdoc/>
     public override long Callback(Node from, Node to)
     {
         ArgumentNullException.ThrowIfNull(from, nameof(from));
         ArgumentNullException.ThrowIfNull(to, nameof(to));
-        
+
         var fromLocation = from.GetLocation();
         var toLocation = to.GetLocation();
-        
-        // Arbitrary nodes have a duration of 0.
+
+        // Arbitrary nodes have a distance of 0.
         if (fromLocation is null || toLocation is null)
         {
             return 0;
         }
         
-        // The duration between the same location is 0.
+        // The distance between the same location is 0.
         if (fromLocation.Equals(toLocation))
         {
             return 0;
         }
-        
+
         return _routeMatrix.GetEdge(fromLocation, toLocation) switch
         {
-            DefinedRouteEdge definedRouteEdge => definedRouteEdge.Duration,
+            DefinedRouteEdge definedRouteEdge => definedRouteEdge.Distance,
             _ => long.MaxValue
         };
     }
